6eafef3016a9b2582849b876a725c8ac34a70aba
Kent Ketter <kent.ketter1@gmail.com>
2019-01-27 15:51:31 -0800

    DbContext Registration Issues

---
+++ AsyncInn/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Controllers
+{
+    /// <summary>
+    /// This method renders the main page on startup
+    /// </summary>
+    public class HomeController: Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AsyncInn/Data/AsyncHotelDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace AsyncInn.Data
+{
+    public class AsyncInnDbContext : DbContext
+    {
+        public AsyncInnDbContext(DbContextOptions<AsyncInnDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
